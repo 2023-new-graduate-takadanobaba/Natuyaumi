5ce8013729653bc2a5cbc7c95e1013cfd4a33631
kakeru ono <kakeru ono@host.docker.internal>
2023-06-07 14:51:02 +0900

    コントローラーとtopの変更

--- natuyasumi/src/main/java/jp/co/sss/natuyasumi/controller/ArticleController.java
+++ natuyasumi/src/main/java/jp/co/sss/natuyasumi/controller/ArticleController.java
@@ -2,6 +2,7 @@ package jp.co.sss.natuyasumi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 
@@ -11,4 +12,19 @@ public class ArticleController {
 		return "top";	
 	}
 	
+	@RequestMapping(path = "/doPost")
+	public String post() {
+		return "redirect:/post";
+	}
+	
+	
+	@RequestMapping(path = "/doSearchGenre", method = RequestMethod.GET)
+	public String genre() {
+		return "redirect:/genre";
+	}
+	
+	@RequestMapping(path = "/doDisplayAirticle", method = RequestMethod.GET)
+	public String airticle() {
+		return "redirect:/airticle";
+	}
 }
