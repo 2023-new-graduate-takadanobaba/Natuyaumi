33c64ff2faa33961d91bfec15409655a22693619
rina.ozone <rina.ozone@host.docker.internal>
2023-06-14 13:51:46 +0900

    postFormだお

--- natuyasumi/src/main/java/jp/co/sss/natuyasumi/form/PostForm.java
+++ natuyasumi/src/main/java/jp/co/sss/natuyasumi/form/PostForm.java
@@ -1,5 +1,121 @@
 package jp.co.sss.natuyasumi.form;
 
 public class PostForm {
+	
+	
+//		private Integer id;
+	
+		 private String title;
+		 
 
-}
+		 private String name;
+		 
+	
+		 private String address;
+		 
+		
+		 private Boolean hasParking;
+		 
+		 
+		 private Integer month;
+		 
+		
+		 private String imageUrl;
+		 
+	
+		 private Integer level;
+		 
+
+//		 private Integer genreId;
+		 
+		
+		 private String review;
+		 
+//		public Integer getId() {
+//			return id;
+//		}
+//
+//		public void setId(Integer id) {
+//			this.id = id;
+//		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getAddress() {
+			return address;
+		}
+
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		public Boolean getHasParking() {
+			return hasParking;
+		}
+
+		public void setHasParking(Boolean hasParking) {
+			this.hasParking = hasParking;
+		}
+
+		public Integer getMonth() {
+			return month;
+		}
+
+		public void setMonth(Integer month) {
+			this.month = month;
+		}
+
+		public String getImageUrl() {
+			return imageUrl;
+		}
+
+		public void setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+		}
+
+		public Integer getLevel() {
+			return level;
+		}
+
+		public void setLevel(Integer level) {
+			this.level = level;
+		}
+
+//		public Integer getGenreId() {
+//			return genreId;
+//		}
+	//
+//		public void setGenreId(Integer genreId) {
+//			this.genreId = genreId;
+//		}
+
+		public String getReview() {
+			return review;
+		}
+
+		public void setReview(String review) {
+			this.review = review;
+		}
+
+		
+
+	}
+
+
+
+
+
