180d536e7e40f23195d06f3e05b48d945f48195f
kakeru ono <kakeru ono@host.docker.internal>
2023-06-16 11:32:07 +0900

    ｑ

--- natuyasumi/src/main/java/jp/co/sss/natuyasumi/controller/ArticleController.java
+++ natuyasumi/src/main/java/jp/co/sss/natuyasumi/controller/ArticleController.java
@@ -72,7 +72,6 @@ public class ArticleController {
 //		System.out.println(form.getGenreId());
 		// 1,2,3
 		// String[] [1,2,3]
-//		int max = 10;   //おぞね書いた
 		String[] alt = form.getGenreId().split(",");   //PostFormクラスのgenreIdを呼び出して、それをString型配列altに格納
 		
 		ArticleEntity article = new ArticleEntity();   //ArticleEntityのオブジェクト生成
@@ -91,20 +90,13 @@ public class ArticleController {
 
 //		String型配列altをIntegerに変換してGenreテーブルのIdを探してsetGenreでarticleに格納している
 //		if(article.getGenre1() != null) {
-		
-//		article.setGenre1(genreRepository.getReferenceById(Integer.parseInt(alt[1])));
+		article.setGenre1(genreRepository.getReferenceById(Integer.parseInt(alt[1])));
 //		}
 //		if(article.getGenre2() != null) {
 		
 //		article.setGenre2(genreRepository.getReferenceById(Integer.parseInt(alt[2])));
 //		}
 		
-//		ここから小曽根
-//		for(int i = 0; i  < max; i++) {
-//			if(i<max) {
-//				alt[i] = 
-//			}
-//		}
 //		article.setGenre3(genreRepository.getReferenceById(Integer.parseInt(alt[3])));
 //		article.setGenre4(genreRepository.getReferenceById(Integer.parseInt(alt[4])));
 //		article.setGenre5(genreRepository.getReferenceById(Integer.parseInt(alt[5])));
