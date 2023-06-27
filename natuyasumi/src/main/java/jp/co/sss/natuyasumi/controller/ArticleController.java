4164665c0f9b98af24955d323a977ac7262efeeb
rina.ozone <rina.ozone@host.docker.internal>
2023-06-27 11:32:03 +0900

    6/27.11:31

--- natuyasumi/src/main/java/jp/co/sss/natuyasumi/controller/ArticleController.java
+++ natuyasumi/src/main/java/jp/co/sss/natuyasumi/controller/ArticleController.java
@@ -1,20 +1,19 @@
 package jp.co.sss.natuyasumi.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import jp.co.sss.natuyasumi.entity.ArticleEntity;
-import jp.co.sss.natuyasumi.entity.Genre;
-import jp.co.sss.natuyasumi.form.PostForm;
-import jp.co.sss.natuyasumi.repository.ArticleRepository;
-import jp.co.sss.natuyasumi.repository.GenreRepository;
 
 @Controller
 
 public class ArticleController {
 	// にゃー
+	@RequestMapping(path = "/")
+	 public String top() {
+	 return "top";
+	}
+	
+	
+	
+	
+	
 }
