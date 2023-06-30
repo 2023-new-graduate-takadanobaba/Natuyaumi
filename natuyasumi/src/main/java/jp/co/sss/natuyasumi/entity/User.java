3d68b82f8b17f078dcdfc5e3d6c116e70d962d74
rina.ozone <rina.ozone@host.docker.internal>
2023-06-30 16:15:02 +0900

    苦戦

---
+++ natuyasumi/src/main/java/jp/co/sss/natuyasumi/entity/User.java
@@ -0,0 +1,35 @@
+package jp.co.sss.natuyasumi.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	private Integer user_id;
+	
+	@Column
+	private String user_name;
+
+	public Integer getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	
+
+}
