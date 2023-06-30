6afd0e43c98dfc2f4fed73912d64cd0162659da9
kakeru ono <kakeru ono@host.docker.internal>
2023-06-30 16:17:02 +0900

    aaa

--- natuyasumi/src/main/java/jp/co/sss/natuyasumi/repository/AuthenticationManager.java
+++
@@ -1,9 +0,0 @@
-package jp.co.sss.natuyasumi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import jp.co.sss.natuyasumi.entity.User;
-
-public interface AuthenticationManager extends JpaRepository<User, Integer>{
-
-}
