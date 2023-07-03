456434bf6f888b3ca3cafef049950d3d2cc7eeeb
kakeru ono <kakeru ono@host.docker.internal>
2023-07-03 13:48:47 +0900

    push

--- natuyasumi/src/main/java/jp/co/sss/natuyasumi/controller/indexController.java
+++ natuyasumi/src/main/java/jp/co/sss/natuyasumi/controller/indexController.java
@@ -1,23 +1,30 @@
 package jp.co.sss.natuyasumi.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpSession;
+import jp.co.sss.natuyasumi.form.userForm;
 
 @Controller
-
 public class indexController {
-	@RequestMapping(path = "/index")
-	public String index() {
-		return "index";	
+	
+	@GetMapping(value = "/index")
+	 public String top() {
+	 return "index";
 	}
 	
-	@RequestMapping(path = "/index", method = RequestMethod.POST)
-	public String doLogin(String userId) {
-		System.out.println("ユーザ ID:" + userId);
-		return "top";
+	@PostMapping(value = "/sessionUser")
+	public String sessionUser(userForm form, HttpSession session) {
+		session.setAttribute("sessionName", form.getUserName());
+		return "SessionUserName";
+		
 	}
-	
-	
 
+	@GetMapping(value = "/doLogout")
+	public String doLogout(HttpSession session) {
+		session.invalidate();
+		return "index";
+	}
 }
---
+++ natuyasumi/src/main/java/jp/co/sss/natuyasumi/form/userForm.java
@@ -0,0 +1,15 @@
+package jp.co.sss.natuyasumi.form;
+
+public class userForm {
+
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
