3db40cca529fe6c144f546d68770dd42c2678e46
kakeru ono <kakeru ono@host.docker.internal>
2023-07-06 16:50:03 +0900

    管理者ページの作成

---
+++ natuyasumi/src/main/java/jp/co/sss/natuyasumi/controller/AdministratorController.java
@@ -0,0 +1,37 @@
+package jp.co.sss.natuyasumi.controller;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+import jp.co.sss.natuyasumi.entity.ArticleEntity;
+import jp.co.sss.natuyasumi.repository.ArticleRepository;
+
+@Controller
+public class AdministratorController {
+	@Autowired
+	ArticleRepository repository;
+
+	@RequestMapping(path = "/administrator", method = RequestMethod.GET)
+	 public String DisplayGenre(Model model) {
+		model.addAttribute("articles", repository.findAll());
+	 return "administrator";
+	}
+	
+	@RequestMapping(path = "/displayAirticle/{id}")
+	 public String doDisplayAirticle(@PathVariable Integer id, Model model, HttpSession session) throws IOException{
+		ArticleEntity article = repository.findById(id).get();
+		model.addAttribute("article", article);
+		byte[] encodeBase64 = Base64.getEncoder().encode(article.getImageData());
+	    String base64Encoded = new String(encodeBase64, "UTF-8");
+		model.addAttribute("imageData", base64Encoded);
+	 return "administratorArticle";
+	}
+}
